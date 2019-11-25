ffc037ff268d42a8943ba42a1ef9262c81d491a1
David Dynasty <davidwbell86@gmail.com>
2019-11-24 18:20:52 -0600

    Edited z-loadscreen

--- resources/z-loadscreen/config.js
+++ resources/z-loadscreen/config.js
@@ -1,17 +1,17 @@
 /* Need Help? Join my discord @ discord.gg/yWddFpQ */
 var config = {
     /* In milliseconds */
-    "transitionInterval": 1250,
-    "imgInterval": 8000,
+    "transitionInterval": 500000,
+    "imgInterval": 500000,
     "music": true,
     /* Insert the Youtube Video ID below */
-    "videoID": "jo4-FhqkNwQ",
+    "videoID": "L8MZPc9To2Y",
     /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
-    "musicVolume": 70,
+    "musicVolume": 60,
     "text": {
-        "title": "My Arpee Server",
-        "link": "discord.gg/yeet",
+        "title": "",
+        "link": "",
     },
     /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
-    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png']
+    "images": ['bg0.png']
 }

1642a52b45ca09e3fb03f2023990ed6a8ef2af33
David Dynasty <davidwbell86@gmail.com>
2019-11-29 13:11:12 -0600

    changed guns and prices in blackmarket

--- resources/grangeillegal/config.lua
+++ resources/grangeillegal/config.lua
@@ -1,16 +1,16 @@
 Config = {}
 
-Config.ArgentSale = true  --true if use of black money, false for regular money
+Config.ArgentSale = false  --true if use of black money, false for regular money
 
 Config.Weapons = {
-    {weapon = "WEAPON_CROWBAR", price = 115},
-    {weapon = "WEAPON_DAGGER", price = 200},
+    {weapon = "weapon_assaultrifle", price = 250000},
+    {weapon = "weapon_sawnoffshotgun", price = 150000},
     {weapon = "WEAPON_MOLOTOV", price = 2750},    
     {weapon = "WEAPON_PISTOL50", price = 12800},
     {weapon = "WEAPON_DOUBLEACTION", price = 16200},
     {weapon = "WEAPON_SAWNOFFSHOTGUN", price = 22400},
-    {weapon = "WEAPON_MINISMG", price = 24800},
-    {weapon = "WEAPON_GUSENBERG", price = 42500}, 
+    {weapon = "weapon_smg", price = 200000},
+    {weapon = "weapon_compactrifle", price = 210000}, 
 }
 
 --Weapon List : https://wiki.rage.mp/index.php?title=Weapons

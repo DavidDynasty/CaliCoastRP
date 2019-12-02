0483c3e90ffa96798bd6b65b808b7d7d7b980f0f
David Dynasty <davidwbell86@gmail.com>
2019-12-01 21:46:54 -0600

    added lights and more to the night club. "more to come"

--- resources/[esx]/esx_nightclubaddon/client/nightclub.lua
+++ resources/[esx]/esx_nightclubaddon/client/nightclub.lua
@@ -13,13 +13,13 @@ Citizen.CreateThread(function()
     AfterHoursNightclubs.Ipl.Interior.Load()
     
     -- Setting the name of the club to The Palace
-    AfterHoursNightclubs.Interior.Name.Set(AfterHoursNightclubs.Interior.Name.studio, true)
+    AfterHoursNightclubs.Interior.Name.Set(AfterHoursNightclubs.Interior.Name.galaxy, true)
 
     -- Glamorous style
-    AfterHoursNightclubs.Interior.Style.Set(AfterHoursNightclubs.Interior.Style.glam, true)
+    AfterHoursNightclubs.Interior.Style.Set(AfterHoursNightclubs.Interior.Style.edgy, true)
 
     -- Glam podiums
-    AfterHoursNightclubs.Interior.Podium.Set(AfterHoursNightclubs.Interior.Podium.glam, true)
+    AfterHoursNightclubs.Interior.Podium.Set(AfterHoursNightclubs.Interior.Podium.edgy, true)
 
     -- speakers
     AfterHoursNightclubs.Interior.Speakers.Set(AfterHoursNightclubs.Interior.Speakers.upgrade, true)
@@ -33,21 +33,25 @@ Citizen.CreateThread(function()
     -- Lights --
 	
 	--Laser--
-    AfterHoursNightclubs.Interior.Lights.Lasers.Set(AfterHoursNightclubs.Interior.Lights.Lasers.cyan, true)
+    AfterHoursNightclubs.Interior.Lights.Lasers.Set(AfterHoursNightclubs.Interior.Lights.Lasers.white, true)
     AfterHoursNightclubs.Interior.Lights.Lasers.Set(AfterHoursNightclubs.Interior.Lights.Lasers.yellow, true)
 	AfterHoursNightclubs.Interior.Lights.Lasers.Set(AfterHoursNightclubs.Interior.Lights.Lasers.purple, true)
 	AfterHoursNightclubs.Interior.Lights.Lasers.Set(AfterHoursNightclubs.Interior.Lights.Lasers.green, true)
 	--Bands--
 	AfterHoursNightclubs.Interior.Lights.Bands.Set(AfterHoursNightclubs.Interior.Lights.Bands.white, true)
 	AfterHoursNightclubs.Interior.Lights.Bands.Set(AfterHoursNightclubs.Interior.Lights.Bands.green, true)
-	AfterHoursNightclubs.Interior.Lights.Bands.Set(AfterHoursNightclubs.Interior.Lights.Bands.yellow, true)
+    AfterHoursNightclubs.Interior.Lights.Bands.Set(AfterHoursNightclubs.Interior.Lights.Bands.yellow, true)
+    AfterHoursNightclubs.Interior.Lights.Bands.Set(AfterHoursNightclubs.Interior.Lights.Bands.cyan, true)
    --Neons--
     AfterHoursNightclubs.Interior.Lights.Neons.Set(AfterHoursNightclubs.Interior.Lights.Neons.yellow, true)
-	AfterHoursNightclubs.Interior.Lights.Neons.Set(AfterHoursNightclubs.Interior.Lights.Neons.purple, true)
-	--Drops--
+    AfterHoursNightclubs.Interior.Lights.Neons.Set(AfterHoursNightclubs.Interior.Lights.Neons.purple, true)
+    AfterHoursNightclubs.Interior.Lights.Neons.Set(AfterHoursNightclubs.Interior.Lights.Neons.white, true)
+    AfterHoursNightclubs.Interior.Lights.Neons.Set(AfterHoursNightclubs.Interior.Lights.Neons.cyan, true)
+	--Droplets--
 	AfterHoursNightclubs.Interior.Lights.Droplets.Set(AfterHoursNightclubs.Interior.Lights.Droplets.green, true)
 	AfterHoursNightclubs.Interior.Lights.Droplets.Set(AfterHoursNightclubs.Interior.Lights.Droplets.purple, true)
-	AfterHoursNightclubs.Interior.Lights.Droplets.Set(AfterHoursNightclubs.Interior.Lights.Droplets.yellow, true)
+    AfterHoursNightclubs.Interior.Lights.Droplets.Set(AfterHoursNightclubs.Interior.Lights.Droplets.yellow, true)
+    AfterHoursNightclubs.Interior.Lights.Droplets.Set(AfterHoursNightclubs.Interior.Lights.Droplets.white,  true)
    
    -- Details
    --AfterHoursNightclubs.Interior.Details.Enable(true)
@@ -58,14 +62,43 @@ Citizen.CreateThread(function()
     AfterHoursNightclubs.Interior.Bar.Enable(true)
 
     -- Enabling all booze bottles
-    AfterHoursNightclubs.Interior.Booze.Enable(AfterHoursNightclubs.Interior.Booze, true)
+    AfterHoursNightclubs.Interior.Booze.Enable(AfterHoursNightclubs.Interior.Booze.C, true)
 
     -- Adding trophies on the desk
-    AfterHoursNightclubs.Interior.Trophy.Enable(AfterHoursNightclubs.Interior.Trophy.dancer, true, AfterHoursNightclubs.Interior.Trophy.Color.gold)
+    AfterHoursNightclubs.Interior.Trophy.Enable(AfterHoursNightclubs.Interior.Trophy.battler, true, AfterHoursNightclubs.Interior.Trophy.Color.gold)
     
     -- Refreshing interior to apply changes
     RefreshInterior(AfterHoursNightclubs.interiorId)
 
+    -- Dry ice
+    AfterHoursNightclubs.Interior.DryIce.Enable(true)
+
+    -- Chest on VIP desk
+    AfterHoursNightclubs.Interior.Details.Enable(AfterHoursNightclubs.Interior.Details.chest, true)
+
+    -- Work lamps + trash
+    AfterHoursNightclubs.Interior.Details.Enable(AfterHoursNightclubs.Interior.Details.worklamps, false)
+
+    -- Floor lights ment to go with trad style
+    AfterHoursNightclubs.Interior.Details.Enable(AfterHoursNightclubs.Interior.Details.floorTradLights, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(AfterHoursNightclubs.Interior.Details.vaultAmmunations, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(AfterHoursNightclubs.Interior.Details.vaultCash, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(details, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(details, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(details, true)
+
+    --
+    AfterHoursNightclubs.Interior.Details.Enable(details, true)
 
     -------------------------------------------
     -- Exterior part

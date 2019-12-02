5905e96695fb3f710e528a8343f99046f3904d80
David Dynasty <davidwbell86@gmail.com>
2019-12-01 22:09:37 -0600

    custom script just for blips. "oh yeah"

---
+++ resources/blips/blips.lua
@@ -0,0 +1,21 @@
+local blips = {
+    -- Example {title="", colour=, id=, x=, y=, z=},
+
+     {title="NightClub", colour=83, id=590, x = 192.21, y = -3166.95, z = 5.79}
+     --{title="Example 2", colour=30, id=108, x = 260.130, y = 204.308, z = 109.287}
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 0.7)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)

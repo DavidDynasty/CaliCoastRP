11ab381b403754dc057054869247219f21884c32
David Dynasty <davidwbell86@gmail.com>
2020-04-07 18:32:38 -0500

    removed "to much at this time"

--- resources/[teleports]/houses_tp/__resource.lua
+++
@@ -1,5 +0,0 @@
-client_scripts {'client.lua'}
-
--- Credits:
--- This script is by SimpleX
--- Find Original here: https://forum.fivem.net/t/release-simple-teleporting-v2-5-multiple-teleports-new-color-customization-options/231621

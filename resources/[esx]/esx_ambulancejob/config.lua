dd7f01bf90326297056380889ef6405bd7b5c998
David Dynasty <davidwbell86@gmail.com>
2020-04-12 01:08:56 -0500

    Updated jobs

--- resources/[esx]/esx_ambulancejob/config.lua
+++ resources/[esx]/esx_ambulancejob/config.lua
@@ -1,8 +1,8 @@
 Config                            = {}
 
-Config.DrawDistance               = 100.0
+Config.DrawDistance               = 25.0
 
-Config.Marker                     = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+Config.Marker                     = {type = 27, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 0, a = 100, rotate = false}
 
 Config.ReviveReward               = 700  -- revive reward, set to 0 if you don't want it enabled
 Config.AntiCombatLog              = true -- enable anti-combat logging?
@@ -21,77 +21,76 @@ Config.RemoveItemsAfterRPDeath    = true
 
 -- Let the player pay for respawning early, only if he can afford it.
 Config.EarlyRespawnFine           = true
-Config.EarlyRespawnFineAmount     = 5000
+Config.EarlyRespawnFineAmount     = 2500
 
-Config.RespawnPoint = {coords = vector3(341.0, -1397.3, 32.5), heading = 48.5}
+Config.RespawnPoint = {coords = vector3(365.92, -582.63, 43.28), heading = 119.71}
 
 Config.Hospitals = {
 
 	CentralLosSantos = {
 
 		Blip = {
-			coords = vector3(307.7, -1433.4, 28.9),
+			coords = vector3(300.19, -584.99, 43.28),
 			sprite = 61,
-			scale  = 1.2,
+			scale  = 1.0,
 			color  = 2
 		},
 
 		AmbulanceActions = {
-			vector3(270.5, -1363.0, 23.5)
+			vector3(298.850, -598.200, 42.290),
 		},
 
 		Pharmacies = {
-			vector3(230.1, -1366.1, 38.5)
+			vector3(356.000, -601.900, 42.290)
 		},
 
 		Vehicles = {
 			{
-				Spawner = vector3(307.7, -1433.4, 30.0),
-				InsideShop = vector3(446.7, -1355.6, 43.5),
-				Marker = {type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 50, b = 200, a = 100, rotate = true},
+				Spawner = vector3(334.400, -560.800, 28.700),
+				InsideShop = vector3(329.85, -555.61, 28.51),
+				Marker = {type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 0, b = 0, a = 100, rotate = true},
 				SpawnPoints = {
-					{coords = vector3(297.2, -1429.5, 29.8), heading = 227.6, radius = 4.0},
-					{coords = vector3(294.0, -1433.1, 29.8), heading = 227.6, radius = 4.0},
-					{coords = vector3(309.4, -1442.5, 29.8), heading = 227.6, radius = 6.0}
+					{coords = vector3(337.95, -559.39, 28.51), heading = 340.57, radius = 4.0},
+					{coords = vector3(341.26, -560.56, 28.51), heading = 340.41, radius = 4.0},
+					{coords = vector3(344.46, -561.71, 28.51), heading = 340.24, radius = 6.0}
 				}
 			}
 		},
 
 		Helicopters = {
 			{
-				Spawner = vector3(317.5, -1449.5, 46.5),
-				InsideShop = vector3(305.6, -1419.7, 41.5),
+				Spawner = vector3(338.52, -587.86, 74.17),
+				InsideShop = vector3(351.3, -588.04, 74.17),
 				Marker = {type = 34, x = 1.5, y = 1.5, z = 1.5, r = 100, g = 150, b = 150, a = 100, rotate = true},
 				SpawnPoints = {
-					{coords = vector3(313.5, -1465.1, 46.5), heading = 142.7, radius = 10.0},
-					{coords = vector3(299.5, -1453.2, 46.5), heading = 142.7, radius = 10.0}
+					{coords = vector3(350.6, -588.04, 74.17), heading = 339.88, radius = 10.0}
 				}
 			}
 		},
 
 		FastTravels = {
 			{
-				From = vector3(294.7, -1448.1, 29.0),
-				To = {coords = vector3(272.8, -1358.8, 23.5), heading = 0.0},
-				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				From = vector3(333.10, -568.4, 42.290),
+				To = {coords = vector3(318.29, -556.48, 28.74), heading = 267.05},
+				Marker = {type = 27, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 0, a = 100, rotate = false}
 			},
 
 			{
-				From = vector3(275.3, -1361, 23.5),
-				To = {coords = vector3(295.8, -1446.5, 28.9), heading = 0.0},
-				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				From = vector3(320.10, -560.80, 27.750),
+				To = {coords = vector3(332.07, -571.6, 43.28), heading = 154.4},
+				Marker = {type = 27, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 0, a = 100, rotate = false}
 			},
 
 			{
-				From = vector3(247.3, -1371.5, 23.5),
-				To = {coords = vector3(333.1, -1434.9, 45.5), heading = 138.6},
-				Marker = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				From = vector3(330.650, -601.14, 42.300),
+				To = {coords = vector3(357.56, -597.2, 28.63), heading = 249.31},
+				Marker = {type = 27, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 0, a = 100, rotate = false}
 			},
 
 			{
-				From = vector3(335.5, -1432.0, 45.50),
-				To = {coords = vector3(249.1, -1369.6, 23.5), heading = 0.0},
-				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				From = vector3(354.900, -596.100, 27.800),
+				To = {coords = vector3(328.2, -600.34, 43.28), heading = 333.58},
+				Marker = {type = 27, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 0, a = 100, rotate = false}
 			},
 
 			{

519fde8fa0f93f4157f84ba2cd58d54e1f1d4d77
David Dynasty <davidwbell86@gmail.com>
2020-04-11 20:00:28 -0500

    added elerts for police

---
+++ resources/[esx]/esx_outlawalert/README.md
@@ -0,0 +1,25 @@
+# esx_outlawalert
+
+Outlaw Alert is an resource that detects if the player either steals an vehicle, fires a weapon or if the player is fighting another player / ped.
+
+Fork of the original resource which has the following changes: made esx compatible, it shows alert to cops when shooting, carjacking or fighting.
+
+# Why is this fork different?
+
+This fork adds chat support along with mythic notifications.
+You must install mythic_notify for this to work.
+
+You can toggle mythic notifications and the chat notifications through the config.
+
+In addition, you can receive support here: https://discord.gg/XsUvj4A
+
+## Requirements
+
+You need [mythic_notify] https://github.com/mythicrp/mythic_notify
+
+## Credits
+
+- nynjardin, original developer
+- JB, initial support for ESX
+- Jijamik, French translation
+- Hawaii_Beach, resource cleanup
---
+++ resources/[esx]/esx_outlawalert/config.lua
@@ -0,0 +1,44 @@
+Config = {}
+
+Config.Locale = 'en'
+
+-- Set the time (in minutes) during the player is outlaw
+Config.Timer = 2
+
+-- Set if show alert when player use gun
+Config.GunshotAlert = true
+
+-- Set if show when player do carjacking
+Config.CarJackingAlert = true
+
+-- Set if show when player fight in melee
+Config.MeleeAlert = false
+
+-- In seconds
+Config.BlipGunTime = 20
+
+-- Blip radius, in float value!
+Config.BlipGunRadius = 200.0
+
+-- In seconds
+Config.BlipMeleeTime = 30
+
+-- Blip radius, in float value!
+Config.BlipMeleeRadius = 50.0
+
+-- In seconds
+Config.BlipJackingTime = 30
+
+-- Blip radius, in float value!
+Config.BlipJackingRadius = 200.0
+
+-- Show notification when cops steal too?
+Config.ShowCopsMisbehave = true
+
+-- Jobs in this table are considered as cops
+Config.WhitelistedCops = {
+	'police'
+}
+
+Config.UseMythic = true
+Config.UseChat = false

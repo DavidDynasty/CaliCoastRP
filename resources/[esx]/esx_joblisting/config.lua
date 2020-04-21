2baa245c24f8fa391f38def0ec7a4ab65d656369
David Dynasty <davidwbell86@gmail.com>
2020-04-20 22:39:30 -0500

    updated joblisting

--- resources/[esx]/esx_joblisting/config.lua
+++ resources/[esx]/esx_joblisting/config.lua
@@ -2,11 +2,11 @@ Config              = {}
 
 Config.DrawDistance = 100.0
 Config.ZoneSize     = {x = 3.0, y = 3.0, z = 0.5}
-Config.MarkerColor  = {r = 25, g = 100, b = 0}
-Config.MarkerType   = 27
+Config.MarkerColor  = {r = 100, g = 100, b = 204}
+Config.MarkerType   = 1
 
-Config.Locale       = 'en'
+Config.Locale       = 'fr'
 
 Config.Zones = {
-	vector3(-265.0, -963.6, 30.230)
+	vector3(-265.0, -963.6, 30.2)
 }

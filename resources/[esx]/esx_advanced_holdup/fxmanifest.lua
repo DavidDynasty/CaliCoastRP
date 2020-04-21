863b79f4a7ee10c8350e170789b4cc3e29d00f4b
David Dynasty <davidwbell86@gmail.com>
2020-04-20 22:34:31 -0500

    added esx_advanced_holdup

---
+++ resources/[esx]/esx_advanced_holdup/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Advanced Holdup'
+
+version '1.0.0'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua',
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}

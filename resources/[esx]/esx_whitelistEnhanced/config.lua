f7241c87bb8114d7368c039f89b13492d4e7de08
David Dynasty <davidwbell86@gmail.com>
2020-04-23 15:08:27 -0500

    Made the waiting timer 0

--- resources/[esx]/esx_whitelistEnhanced/config.lua
+++ resources/[esx]/esx_whitelistEnhanced/config.lua
@@ -3,6 +3,6 @@ Config = {}
 Config.Locale = 'en'
 
 Config.EnableAntiSpam = true
-Config.WaitingTime = 1
+Config.WaitingTime = 0
 Config.CommunityLink = 'CaliCoastRP Discord'
 Config.PlayersToStartRocade = 32

36144945d11a8cb96b158a9a813506d65700d400
David Dynasty <davidwbell86@gmail.com>
2020-05-02 08:35:32 -0500

    added esx_garage

--- resources/[esx]/esx_garbage/client/main.lua
+++ resources/[esx]/esx_garbage/client/main.lua
@@ -820,7 +820,7 @@ Citizen.CreateThread(function()
 
 	SetBlipSprite (blip, 318)
 	SetBlipDisplay(blip, 4)
-	SetBlipScale  (blip, 1.2)
+	SetBlipScale  (blip, 0.7)
 	SetBlipColour (blip, 5)
 	SetBlipAsShortRange(blip, true)
 

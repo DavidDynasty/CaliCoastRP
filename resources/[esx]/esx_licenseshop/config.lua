7df4a19806780dc266533d3d55189688ac0e43df
David Dynasty <davidwbell86@gmail.com>
2020-05-02 19:37:49 -0500

    edited licenseshop and lscustom

--- resources/[esx]/esx_licenseshop/config.lua
+++ resources/[esx]/esx_licenseshop/config.lua
@@ -4,12 +4,12 @@ Config.Locale = 'en'
 Config.MenuAlign = 'top-left'
 Config.DrawDistance = 100
 
-Config.MarkerInfo = {Type = 1, r = 102, g = 102, b = 204, x = 2.0, y = 2.0, z = 1.0}
-Config.BlipLicenseShop = {Sprite = 408, Color = 0, Display = 2, Scale = 1.0}
+Config.MarkerInfo = {Type = 27, r = 102, g = 102, b = 204, x = 2.0, y = 2.0, z = 1.0}
+Config.BlipLicenseShop = {Sprite = 498, Color = 64, Display = 2, Scale = 0.7}
 
-Config.UseBlips = false -- true = Use License Shop Blips
+Config.UseBlips = true -- true = Use License Shop Blips
 Config.EnablePeds = true -- If true then it will add Peds on Markers | false does the Opposite.
-Config.RequireDMV = false -- If true then it will require players to have Drivers Permit to buy other Licenses | false does the Opposite.
+Config.RequireDMV = true -- If true then it will require players to have Drivers Permit to buy other Licenses | false does the Opposite.
 
 Config.AdvancedVehicleShop = true -- Set to True if using esx_advancedvehicleshop
 Config.LicenseAircraft = 10000
@@ -24,7 +24,7 @@ Config.LicenseAssault = 250
 Config.LicenseLMG = 1000
 Config.LicenseSniper = 1500
 
-Config.DMVSchool = false -- Set to true if using esx_dmvschool
+Config.DMVSchool = true -- Set to true if using esx_dmvschool
 Config.SellDMV = false -- Set to true if Config.RequireDMV = false & you want players to be able to buy Drivers Permit
 Config.LicenseCommercial = 300
 Config.LicenseDrivers = 150
@@ -34,16 +34,16 @@ Config.LicenseMotocycle = 225
 Config.Drugs = false -- Set to true if using esx_drugs
 Config.LicenseWeed = 10000
 
-Config.WeaponShop = false -- Set to true if using esx_weaponshop
+Config.WeaponShop = true -- Set to true if using esx_weaponshop
 Config.LicenseWeapon = 1000
 
 Config.Zones = {
 	LicenseShop1 = { -- Next to esx_dmvschool
-		Pos = vector3(241.0, -1378.9, 32.7),
-		Heading = 148.29
+		Pos = vector3(-926.98, -2035.56, 8.410),
+		Heading = 226.8
 	},
-	LicenseShop2 = { -- In LSPD Lobby
+	--[[LicenseShop2 = { -- In LSPD Lobby
 		Pos = vector3(448.6, -988.75, 29.69),
 		Heading = 25.05
-	}
+	}]]--
 }
--- resources/[esx]/esx_lscustom/server/main.lua
+++ resources/[esx]/esx_lscustom/server/main.lua
@@ -60,7 +60,7 @@ end)
 
 ESX.RegisterServerCallback('esx_lscustom:getVehiclesPrices', function(source, cb)
 	if not Vehicles then
-		MySQL.Async.fetchAll('SELECT * FROM vehicles', {}, function(result)
+		MySQL.Async.fetchAll('SELECT * FROM vs_cars', {}, function(result)
 			local vehicles = {}
 
 			for i=1, #result, 1 do
@@ -72,6 +72,88 @@ ESX.RegisterServerCallback('esx_lscustom:getVehiclesPrices', function(source, cb
 
 			Vehicles = vehicles
 			cb(Vehicles)
+		end)
+	else
+		cb(Vehicles)
+	end
+end)
+
+-- Example if you are using all Vehicle Shops
+ESX.RegisterServerCallback('esx_lscustom:getVehiclesPrices', function(source, cb)
+	if not Vehicles then
+	    local vehicles = {}
+
+		-- Get Ambulance Vehicles
+		MySQL.Async.fetchAll('SELECT * FROM vs_ambulance', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get Police Vehicles
+		MySQL.Async.fetchAll('SELECT * FROM vs_police', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get Aircrafts
+		MySQL.Async.fetchAll('SELECT * FROM vs_aircrafts', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get Boats
+		MySQL.Async.fetchAll('SELECT * FROM vs_boats', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get Cars
+		MySQL.Async.fetchAll('SELECT * FROM vs_cars', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get Trucks
+		MySQL.Async.fetchAll('SELECT * FROM vs_trucks', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+		end)
+
+		-- Get VIPS
+		MySQL.Async.fetchAll('SELECT * FROM vs_vips', {}, function(result)
+			for i=1, #result, 1 do
+				table.insert(vehicles, {
+					model = result[i].model,
+					price = result[i].price
+				})
+			end
+
+			Vehicles = vehicles 
+			cb(Vehicles)             
 		end)
 	else
 		cb(Vehicles)

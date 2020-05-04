c44b9062236d23f8ac303e1216699278dd14bae8
David Dynasty <davidwbell86@gmail.com>
2020-05-03 21:25:43 -0500

    added esx_teleportpads

---
+++ resources/[esx]/esx_teleportpads/config.lua
@@ -0,0 +1,91 @@
+Config = {}
+
+Config.DrawDistance = 100.0
+
+Config.Pads = {
+
+	{ -- Coke Lab Enter
+		Text = '~INPUT_CONTEXT~ ~r~Enter Garage Door~s~',
+		Marker = vector3(-133.45, 6165.71, 30.0),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(1102.54, -3196.2, -38.99), h = 96.2 }
+	},
+
+	{ -- Coke Lab Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(1103.59, -3195.94, -40.10),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(-132.54, 6164.71, 31.02), h = 221.38 }
+	},
+
+	{ -- Weed Lab Enter
+		Text = '~INPUT_CONTEXT~ ~r~Enter~s~',
+		Marker = vector3(1754.01, -1649.15, 111.60),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(1064.11, -3183.72, -39.16), h = 130.08 }
+	},
+
+	{ -- Weed Lab Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(1066.38, -3183.46, -40.20),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(1755.89, -1649.04, 112.66), h = 242.62 }
+	},
+
+	{ -- Meth Lab Enter
+		Text = '~INPUT_CONTEXT~ ~r~Enter~s~',
+		Marker = vector3(916.46, 3576.95, 32.50),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(998.13, -3200.55, -36.39), h = 343.93 }
+	},
+
+	{ -- Meth Lab Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(996.91, -3200.74, -37.50),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(918.07, 3576.81, 33.57), h = 353.51 }
+	},
+
+	{ -- ClubHouse 1 Enter
+		Text = '~INPUT_CONTEXT~ ~w~Enter~s~',
+		Marker = vector3(-1122.14, 2696.83, 17.50),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(1120.97, -3151.43, -37.06), h = 21.21 }
+	},
+
+	{ -- ClubHouse 1 Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(1121.08, -3152.77, -38.20),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(-1119.98, 2696.02, 18.55), h = 252.57 }
+	},
+
+	{ -- ClubHouse 2 Enter
+		Text = '~INPUT_CONTEXT~ ~w~Enter~s~',
+		Marker = vector3(758.99, -697.23, 27.60),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(998.34, -3158.34, -38.91), h = 248.54 }
+	},
+
+	{ -- ClubHouse 2 Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(996.99, -3158.05, -40.20),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(760.73, -697.28, 28.71), h = 269.86 }
+	},
+
+	{ -- Money Cleaning Enter
+		Text = '~INPUT_CONTEXT~ ~r~Enter~s~',
+		Marker = vector3(-1289.99, -820.31, 13.50),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(1137.97, -3197.61, -39.67), h = 74.17 }
+	},
+
+	{ -- Money Cleaning Exit
+		Text = '~INPUT_CONTEXT~ ~w~Leave~s~',
+		Marker = vector3(1138.05, -3199.18, -40.70),
+		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 27, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(-1288.6, -821.9, 15.45), h = 225.19 }
+	}
+
+}
